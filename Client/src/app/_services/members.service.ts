e348f56d578a45d0b5311f2b969fb9d2736f9a9e
NazarPetrakov <nazarpetrakov1@gmail.com>
2024-12-02 23:14:49 +0200

    End of Section 11

--- Client/src/app/_services/members.service.ts
+++ Client/src/app/_services/members.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { Member } from '../_models/member';
 import { environment } from '../../environments/environment.development';
 import { of, tap } from 'rxjs';
+import { Photo } from '../_models/photo';
 
 @Injectable({
   providedIn: 'root',
@@ -11,26 +12,56 @@ export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
   members = signal<Member[]>([]);
-  
+
   getMembers() {
-    return this.http.get<Member[]>(
-      this.baseUrl + 'users').subscribe({
-        next: members=> this.members.set(members)
-      });
+    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
+      next: (members) => this.members.set(members),
+    });
   }
   getMember(username: string) {
-    const member = this.members().find(x => x.userName === username)
-    if(member !== undefined) return of(member)
-    return (
-      this.http.get<Member>(this.baseUrl + 'users/' + username)
-    );
+    const member = this.members().find((x) => x.userName === username);
+    if (member !== undefined) return of(member);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
-  updateMember(member: Member){
+  updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
-      tap(() => { 
-        this.members.update(members => members.map(m => m.userName === member.userName ? member : m))
+      tap(() => {
+        this.members.update((members) =>
+          members.map((m) => (m.userName === member.userName ? member : m))
+        );
       })
     );
   }
-
+  deletePhoto(photo: Photo) {
+    return this.http
+      .delete(this.baseUrl + 'users/delete-photo/' + photo.id)
+      .pipe(
+        tap(() => {
+          this.members.update((members) =>
+            members.map((m) => {
+              if (m.photos.includes(photo)) {
+                m.photos = m.photos.filter((x) => x.id !== photo.id);
+              }
+              return m;
+            })
+          );
+        })
+      );
+  }
+  setMainPhoto(photo: Photo) {
+    return this.http
+      .put(this.baseUrl + 'users/set-main-photo/' + photo.id, {})
+      .pipe(
+        tap(() => {
+          this.members.update((members) =>
+            members.map((m) => {
+              if (m.photos.includes(photo)) {
+                m.photoUrl = photo.url;
+              }
+              return m;
+            })
+          );
+        })
+      );
+  }
 }
